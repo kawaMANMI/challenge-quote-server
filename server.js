80b07fc9e3a774919552c2a783b68f0d9979f130
KawaManmi <kawamam2007@gmail.com>
2022-12-23 22:19:35 +0000

    Want to push it

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
+const lodash = require('lodash');
+const cors = require('cors');
 const app = express();
 
 //load the quotes JSON
@@ -12,26 +14,40 @@ const Quotes = require("./quotes.json");
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
-app.get("/", function(request, response) {
+app.get("/", function (request, response) {
   response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
-
-
-
 //START OF YOUR CODE...
-app.get("/quotes", (req, res)=> {
-  res.json(Quotes)
-})
+app.get("/quotes", (req, res) => {
+  res.json(Quotes);
+});
 
+app.get("/quotes/random", (req, res) => {
+  // res.json(pickFromArray(Quotes));
+  res.json(lodash.sample(Quotes));
+});
 
-app.get("/quotes/random", (req, res)=> {
-  res.json(pickFromArray(Quotes))
-})
+app.get("/quotes/search", (req, res) => {
+  // res.json(
+  //   Quotes.filter(
+  //     (elmQuote) =>
+  //       elmQuote["quote"].toLowerCase().includes(req.params.term) ||
+  //       elmQuote["author"].toLowerCase().includes(req.params.term)
+  //   )
+  // );
+//This example can be done using either parametrs or qurey 
+// console.log(req.query.term)
+// console.log(req.query.key)
 
-app.get("/quotes/:term", (req, res)=> {
-  res.json( Quotes.filter(elmQuote => elmQuote['quote'].toLowerCase().includes(req.params.term) || elmQuote['author'].toLowerCase().includes(req.params.term)))
-})
+  res.json(
+    Quotes.filter(
+      (elmQuote) =>
+        elmQuote["quote"].toLowerCase().includes(req.query.term) ||
+        elmQuote["author"].toLowerCase().includes(req.query.term)
+    )
+  );
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
@@ -48,6 +64,7 @@ function pickFromArray(arr) {
 //   console.log("Your app is listening on port " + PORT);
 // });
 
+app.use(cors());
 
-const PORT=process.env.PORT || 5000;
-app.listen(PORT, ()=> console.log(`server started on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
