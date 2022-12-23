8eaead36fd3c1f3adef99319f3861709563a086b
KawaManmi <kawamam2007@gmail.com>
2022-12-23 19:44:40 +0000

    Search parametr has been added using filter of array

---
+++ server.js
@@ -0,0 +1,53 @@
+// server.js
+// This is where your node app starts
+
+//load the 'express' module which makes writing webservers easy
+const express = require("express");
+const app = express();
+
+//load the quotes JSON
+const Quotes = require("./quotes.json");
+
+// Now register handlers for some routes:
+//   /                  - Return some helpful welcome info (text)
+//   /quotes            - Should return all quotes (json)
+//   /quotes/random     - Should return ONE quote (json)
+app.get("/", function(request, response) {
+  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+});
+
+
+
+
+//START OF YOUR CODE...
+app.get("/quotes", (req, res)=> {
+  res.json(Quotes)
+})
+
+
+app.get("/quotes/random", (req, res)=> {
+  res.json(pickFromArray(Quotes))
+})
+
+app.get("/quotes/:term", (req, res)=> {
+  res.json( Quotes.filter(elmQuote => elmQuote['quote'].toLowerCase().includes(req.params.term) || elmQuote['author'].toLowerCase().includes(req.params.term)))
+})
+//...END OF YOUR CODE
+
+//You can use this function to pick one element at random from a given array
+//example: pickFromArray([1,2,3,4]), or
+//example: pickFromArray(myContactsArray)
+//
+function pickFromArray(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+// const PORT=process.env.PORT || 5000;
+//Start our server so that it listens for HTTP requests!
+// const listener = app.listen(PORT, function() {
+//   console.log("Your app is listening on port " + PORT);
+// });
+
+
+const PORT=process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log(`server started on port ${PORT}`));
